8ef31759a397781a95889150d2de6dd6c1cfaee1
Michael <mbharrat@gmail.com>
2017-10-09 14:24:57 -0500

    fixing price filter to dropdown

--- assets/js/sapp.js
+++ assets/js/sapp.js
@@ -7,6 +7,15 @@ sortSapp.controller('suppController', function($scope, $http) {
 		$scope.supplements = response;
 		alert("supp yay");
 	});
+	//Contains the filter options
+  $scope.filterOptions = {
+    stores: [
+      {id : 2, name : 'Show All', rating: 6 },
+      {id : 3, name : '$', rating: 5 },
+      {id : 4, name : '$$', rating: 4 },
+      {id : 5, name : '$$$', rating: 3 }
+    ]
+  };
 
 
 });
